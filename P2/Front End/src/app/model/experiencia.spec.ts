07663d15031b0a70499da0b8ad7f7d84e945224c
SamyMonetta <samisami_monetta@hotmail.com>
2022-10-31 17:12:19 -0300

    .

---
+++ P2/Front End/src/app/model/experiencia.spec.ts
@@ -0,0 +1,7 @@
+import { ExperienciaComponent } from '../components/experiencia/experiencia.component';
+
+describe('ExperienciaSkipTests', () => {
+  it('should create an instance', () => {
+    expect(new ExperienciaComponent()).toBeTruthy();
+  });
+});
